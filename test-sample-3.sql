83d051fbcbc7664b39719fbc23aee92904742e1e
mihirKachroo <mkachroo29@gmail.com>
2024-06-20 19:56:29 -0400

    update sample 3

--- test-sample-3.sql
+++ test-sample-3.sql
@@ -1,3 +1,7 @@
+-- Example INSERT into rental table information regarding the user renting the listing
+INSERT INTO Rental (rental_id, listing_id, user_id, rent_date, return_date, status)
+VALUES ('7384c7a7', '7315770787', '67cab03c', '2024-06-05', '2024-06-19', 'renting');
+
 -- Example UPDATE listing to archived
 UPDATE Listings
 SET status = 'archived'
