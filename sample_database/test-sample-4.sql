0684e8809dc45c2237610961edc931a09aada259
parshjp <pjparikh1023@gmail.com>
2024-06-20 21:50:23 -0400

    fix

--- sample_database/test-sample-4.sql
+++ sample_database/test-sample-4.sql
@@ -1,7 +1,7 @@
 -- Example of basic search an filter feature to get top 3 good cars in california with price < 50 ordered by posting date
--- only show active listings for rentals and owners will not see their own postings in the main search
+-- only show active listings for rentals
 -- 67cab03c represents the current user's id
 SELECT * FROM Listings
-WHERE `condition` = 'good' AND state_id = 'ca' AND price <= 50 AND owner_id != '67cab03c' AND status = 'active'
+WHERE `condition` = 'good' AND state_id = 'ca' AND price <= 50 AND status = 'active'
 ORDER BY posting_date ASC
 Limit 3;
